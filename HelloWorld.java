0039fbd447fc7a5d2184754a57b76a0332685a69
Rajiv Puli <rajiv.puli@syngenta.com>
2023-11-01 23:21:12 +0530

    Webhook testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("First job success");
+        System.out.println("********Webhook Testing********");
     }
 }
